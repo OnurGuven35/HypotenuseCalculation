7cedab2f8c6842b556c9b42ab9c584caf4503973
OnurGuven35 <gvnn.onrr@gmail.com>
2022-06-12 23:54:06 +0300

    Düzeltme

--- src/Hypo.java
+++ src/Hypo.java
@@ -1,19 +1,26 @@
+// Java ile kullanıcıdan dik kenarlarının uzunluğunu alan ve hipotenüsü hesaplayan programı yazın.
+
 import java.util.Scanner;
 
 public class Hypo {
     public static void main(String[] args) {
+        Scanner hypo = new Scanner(System.in);
 
         int kenarBir, kenarIki;
         double hipotenus;
 
-        Scanner hypo = new Scanner(System.in);
+        // İlk olarak Kullanıcıdan kenar uzunluklarını alıyoruz.
 
+        System.out.println();
         System.out.print("1. Kenari Giriniz : ");
         kenarBir = hypo.nextInt();
         System.out.print("2. Kenari Giriniz : ");
         kenarIki = hypo.nextInt();
 
+        // Daha sonra hipotenüs formülünü yazarak ekrana yazdırıyoruz.
+
         hipotenus = Math.sqrt((kenarBir * kenarBir) + (kenarIki * kenarIki));
-        System.out.print("Hipotenus : " + hipotenus);
+        System.out.println();
+        System.out.println("Hipotenus : " + hipotenus);
     }
 }
